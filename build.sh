67a662638ce56d77e3bc8ea13efe9acc11179909
Timur Fatykhov <tfatykhov@gmail.com>
2017-11-18 22:01:39 -0500

    small update to force pulling latest nginx and letsencrypt

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose stop && docker-compose rm -f && docker build --rm -t tfatykhov/nginx-proxy:1.0 . && docker-compose  up -d && echo "Build prcess completed.. runninng logs" && docker-compose logs -f
+docker-compose stop && docker-compose rm -f && docker build --pull --no-cache --rm -t tfatykhov/nginx-proxy:1.0 . && docker-compose  up -d && echo "Build prcess completed.. runninng logs" && docker-compose logs -f
